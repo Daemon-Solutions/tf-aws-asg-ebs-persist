5c7ee4265120a7eb472aa4139d893a40fab640ac
Pawel Fiuto <pawel@bashton.com>
2017-06-06 15:14:12 +0200

    Clean external data source script

--- lambda/scripts/data.py
+++ lambda/scripts/data.py
@@ -6,12 +6,9 @@ import sys
 import shutil
 import ConfigParser
 
+# read json
 arguments = json.load(sys.stdin)
 
-
-module_path = arguments['module_path']
-stack_name = arguments['stack_name']
-lambda_version = arguments['lambda_version']
 # dicts
 mount_point = json.loads(arguments['mount_point'])
 volume_size = json.loads(arguments['volume_size'])
@@ -19,35 +16,40 @@ volume_type = json.loads(arguments['volume_type'])
 volume_iops = json.loads(arguments['volume_iops'])
 tag_value = json.loads(arguments['tag_value'])
 encrypted = json.loads(arguments['encrypted'])
-#string
+
+# strings
 tag_name = arguments['tag_name']
 time_limit = arguments['time_limit']
-
+module_path = arguments['module_path']
+stack_name = arguments['stack_name']
+lambda_version = arguments['lambda_version']
 
 # manage directories
 stack_dir = os.path.join(module_path, 'files', stack_name)
 if os.path.exists(stack_dir):
-    shutil.rmtree(os.path.join(module_path, 'files', stack_name))
-os.mkdir(os.path.join(module_path, 'files', stack_name))
+    shutil.rmtree(stack_dir)
+os.mkdir(stack_dir)
+
 # copy main script
-shutil.copyfile(os.path.join(module_path, 'scripts', 'main.py'), os.path.join(module_path, 'files', stack_name, 'main.py'))
+shutil.copyfile(os.path.join(module_path, 'scripts', 'main.py'),
+                os.path.join(stack_dir, 'main.py'))
 
+# generate config file
 Config = ConfigParser.ConfigParser()
-
-cfgfile = open(os.path.join(module_path, 'files', stack_name, 'lambda_as_ebs.conf'), 'w')
+cfgfile = open(os.path.join(stack_dir, 'lambda_as_ebs.conf'), 'w')
 for key in mount_point.keys():
-    Config.add_section(mount_point[key])
-    Config.set(mount_point[key], 'time_limit', time_limit)
-    Config.set(mount_point[key], 'volume_size', volume_size[key])
-    Config.set(mount_point[key], 'volume_type', volume_type[key])
-    Config.set(mount_point[key], 'volume_iops', volume_iops[key])
-    Config.set(mount_point[key], 'mount_point', mount_point[key])
-    Config.set(mount_point[key], 'tag_name', tag_name)
-    Config.set(mount_point[key], 'tag_value', tag_value[key])
-    Config.set(mount_point[key], 'encrypted', encrypted[key])
+    section = mount_point[key]
+    Config.add_section(section)
+    Config.set(section, 'time_limit', time_limit)
+    Config.set(section, 'volume_size', volume_size[key])
+    Config.set(section, 'volume_type', volume_type[key])
+    Config.set(section, 'volume_iops', volume_iops[key])
+    Config.set(section, 'mount_point', mount_point[key])
+    Config.set(section, 'tag_name', tag_name)
+    Config.set(section, 'tag_value', tag_value[key])
+    Config.set(section, 'encrypted', encrypted[key])
 Config.write(cfgfile)
 cfgfile.close()
 
-json.dump({
-    "source_dir": os.path.join(module_path, 'files', stack_name),
-}, sys.stdout)
+# return json
+json.dump({"source_dir": stack_dir}, sys.stdout)
