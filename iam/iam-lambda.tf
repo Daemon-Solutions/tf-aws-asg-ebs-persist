c305cd8f086f5975780c2af672a1f0b1cbfc3041
Raymond Butcher <ray.butcher@claranet.uk>
2017-10-02 18:00:27 +0100

    Remove unused instance profile
    The parent module doesn't use the output, so it's being created for no reason and not being used by anything.

--- iam/iam-lambda.tf
+++ iam/iam-lambda.tf
@@ -3,11 +3,6 @@ resource "aws_iam_role" "lambda_iam" {
   assume_role_policy = "${file("${path.module}/assume_role_policy_lambda.json")}"
 }
 
-resource "aws_iam_instance_profile" "lambda_profile" {
-  name  = "iam.${var.envname}.lambda.${var.stack_name}"
-  roles = ["${aws_iam_role.lambda_iam.name}"]
-}
-
 resource "aws_iam_role_policy" "lambda_pol" {
   name   = "policy.${var.envname}.lambda.${var.stack_name}"
   role   = "${aws_iam_role.lambda_iam.id}"
