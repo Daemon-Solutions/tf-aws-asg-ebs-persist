b05270a4a7973754db4f65d7fe4b4a5fc31123e1
Raymond Butcher <ray.butcher@claranet.uk>
2017-10-02 17:58:28 +0100

    Remove non-templates

--- iam/templates.tf
+++
@@ -1,7 +0,0 @@
-data "template_file" "assume_role_policy_lambda" {
-  template = "${file("${path.module}/assume_role_policy_lambda.tpl")}"
-}
-
-data "template_file" "role_policy_lambda" {
-  template = "${file("${path.module}/lambda_policy.tpl")}"
-}
